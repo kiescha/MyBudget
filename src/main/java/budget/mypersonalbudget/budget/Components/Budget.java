b59053a7c3803c18c6511136eddf4e4baa86d61f
Kes <kiestas@gmail.com>
2025-04-14 19:30:50 +0200

    Added Budget and BudgetRecord with basic logic.

---
+++ src/main/java/budget/mypersonalbudget/budget/Components/Budget.java
@@ -0,0 +1,18 @@
+package budget.mypersonalbudget.budget.Components;
+
+import java.util.HashMap;
+
+public class Budget {
+    static final HashMap<Integer, BudgetRecord> recordHashMap = new HashMap<Integer, BudgetRecord>();
+
+
+    private int getNextId() {
+        int highestId = 0;
+        for (Integer key : recordHashMap.keySet()) {
+            if (key > highestId) {
+                highestId = key;
+            }
+        }
+        return highestId + 1;
+    }
+}
---
+++ src/main/java/budget/mypersonalbudget/budget/Components/BudgetRecord.java
@@ -0,0 +1,24 @@
+package budget.mypersonalbudget.budget.Components;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class BudgetRecord {
+    private int id;
+    @Getter
+    double sum;
+    @Getter
+    @Setter
+    String data;
+    @Getter
+    final String info;
+    @Getter
+    final String category;
+
+    public BudgetRecord(double sum, String data, String info, String category) {
+        this.sum = sum;
+        this.data = data;
+        this.info = info;
+        this.category = category;
+    }
+}
