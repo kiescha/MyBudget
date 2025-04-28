6f05718f78d876f3d2fdc8b4fec965a715a40f05
Kes <kiestas@gmail.com>
2025-04-28 18:15:13 +0200

    Added Bank, User, Transaction, TransactionType, Card, Budget, Category, BudgetUser Entity.

--- src/main/java/budget/mypersonalbudget/model/BudgetUserEntity.java
+++ src/main/java/budget/mypersonalbudget/model/BudgetUserEntity.java
@@ -3,5 +3,5 @@ package budget.mypersonalbudget.model;
 public class BudgetUserEntity {
     private long budgetUserId;
     //something for user_id
-    //something for dugbet_id
+    //something for budget_id
 }
